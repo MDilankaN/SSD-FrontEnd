a74a86f63c9ce90a3238260c4eda2384063d83fb
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-02 02:11:58 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Pages/Login';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App flex h-screen justify-center items-center">
       <Login/>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<App/>} />
+      <Route path="home" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
 );
-
