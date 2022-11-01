d6dea57dc6ac506fae6af694669e70943c0e9cb0
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-02 01:55:29 +0530

    login done

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
 function Login() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const submitdata = (e) => {
     e.preventDefault();
-    if (username == "" || password == "") {
+    if (username === "" || password === "") {
       console.log("No values");
 
       return;
@@ -18,29 +17,38 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="w-full  bg-jet text-white text-center px-1 md:px-4 py-2 m-auto rounded md:w-1/2">
+      <h2 className="text-2xl mb-5">Login</h2>
       <div>
         <form onSubmit={(e) => submitdata(e)}>
-          <div>
-            <label>Enter your Username</label>
+          <div className="flex flex-col">
+            <label className="m-1">Enter your Username</label>
             <input
+              className="my-1 mx-auto px-2 w-4/5 py-1 rounded"
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
+            <label className="m-1"></label>
           </div>
 
-          <div>
-            <label>Enter your Password</label>
+          <div className="flex flex-col">
+            <label className="m-1">Enter your Password</label>
             <input
+              className="my-1 mx-auto px-2 w-4/5 py-1 rounded"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+            <label className="m-1"></label>
           </div>
 
-          <button type="submit">Login</button>
+          <button
+            className="text-white bg-smoky-black m-3 px-4 py-1 rounded"
+            type="submit"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
