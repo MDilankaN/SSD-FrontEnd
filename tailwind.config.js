dfaaa510ff95b564a5d44c6b99c2f1bdf7e2971b
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-02 01:55:38 +0530

    color pallet added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors:{
+      transparent: 'transparent',
+      'white': '#ffffff',
+      'black': '#000000',
+      'smoky-black': '#14110f',
+      'jet':'#34312d',
+      'web-gray':'#7e7f83',
+      'desert-sand':'#d9c5b2'
+    },
     extend: {},
   },
   plugins: [],
