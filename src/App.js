5d952fc06740290a8b41ea5a2ffa03a75cff5bd9
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-04 18:28:10 +0530

    add user form completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import Login from './Pages/Login';
+import "./App.css";
+import Login from "./Pages/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import UserRegistration from "./Pages/UserRegistration";
@@ -7,13 +7,13 @@ import UserRegistration from "./Pages/UserRegistration";
 function App() {
   return (
     <div className="App flex h-screen justify-center items-center">
-  <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Login/>} />
-      <Route path="home" element={<Home/>} />
-      <Route path="add-user" element={<UserRegistration/>} />
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="home" element={<Home />} />
+          <Route path="add-user" element={<UserRegistration />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
