678b1a78bd7f153d47a44fb39d5ca761a2f3fd4c
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-11-15 22:49:32 +0530

    commit changes
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -4,10 +4,12 @@ const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InR5cGUiOiJhZG1pbiIsInVzZXJuYW1lIjoiYWRtaW54IiwicGFzc3dvcmQiOiIkMmIkMTAkZzFPTDNIOUJhV2o5QkpiNXJ4WXBhLm9oazBxNzVQUENuaVBTZk1COHcwU2JZTWJJYkN2TVciLCJlbWFpbCI6ImFkbWlueEBnbWFpbC5jb20ifSwiaWF0IjoxNjY3ODQ0Mjg2LCJleHAiOjE2Njc4NDUxODZ9.UpVQ2u4P-IgOo9gYpotU7Rd-2ZhRYFOIbXYQLXggPYs";
 export async function loginUser(data) {
   try {
+    console.log(data)
     const responce = await axios.post(
-      "http://localhost:5000/userRoute/login",
+      "https://localhost:5000/userRoute/login",
       data
     );
+    console.log(responce)
     if(responce.data.accessToken){
       // localStorage.setItem('user')
     }
@@ -22,7 +24,7 @@ export async function loginUser(data) {
 export async function addUser(data) {
   try {
     const responce = await axios.post(
-      "http://localhost:5000/userRoute/register",
+      "https://localhost:5000/userRoute/register",
       data,
       {
         headers: {
