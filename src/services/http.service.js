fcfe68f458ecf214fd3dc0fe7258caa3de87dcf7
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-11-18 16:01:29 +0530

    register validation fixed

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import CryptoJS from 'crypto-js';
+import CryptoJS from "crypto-js";
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InR5cGUiOiJhZG1pbiIsInVzZXJuYW1lIjoiYWRtaW54IiwicGFzc3dvcmQiOiIkMmIkMTAkZzFPTDNIOUJhV2o5QkpiNXJ4WXBhLm9oazBxNzVQUENuaVBTZk1COHcwU2JZTWJJYkN2TVciLCJlbWFpbCI6ImFkbWlueEBnbWFpbC5jb20ifSwiaWF0IjoxNjY3ODQ0Mjg2LCJleHAiOjE2Njc4NDUxODZ9.UpVQ2u4P-IgOo9gYpotU7Rd-2ZhRYFOIbXYQLXggPYs";
 export async function loginUser(data) {
@@ -12,9 +12,9 @@ export async function loginUser(data) {
       localStorage.setItem("token", responce.data.accessToken);
       localStorage.setItem("user", responce.data.user.username);
       localStorage.setItem("type", responce.data.user.type);
-      alert('successful login')
+      alert("successful login");
     } else {
-      alert('Login Failed')
+      alert("Login Failed");
     }
     console.log(responce.data);
 
@@ -35,25 +35,30 @@ export async function addUser(data) {
         },
       }
     );
-    console.log(responce);
-    alert("user added successfully")
+    if (responce.data === "successed") {
+      alert("user added successfully");
+    } else {
+      alert(responce.data);
+    }
+    return responce.data
   } catch (e) {
-    alert("unsuccesful")
+    alert("unsuccesful");
     console.log(e);
   }
 }
 
 //add message
 export async function addMessage(message) {
-
   //encrypt the message
-  const encrypt = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(message));
+  const encrypt = CryptoJS.enc.Base64.stringify(
+    CryptoJS.enc.Utf8.parse(message)
+  );
   console.log("ENCRYPT", encrypt);
   //create son object and send as API Call
   var data = {
-    "username": localStorage.getItem("user"),
-    "message": encrypt
-  }
+    username: localStorage.getItem("user"),
+    message: encrypt,
+  };
   try {
     const responce = await axios.post(
       "https://localhost:5000/messageRoute/sendmessage",
@@ -64,10 +69,10 @@ export async function addMessage(message) {
         },
       }
     );
-    alert("message sent successfully")
+    alert("message sent successfully");
     console.log(responce);
   } catch (e) {
-    alert("unsuccessful")
+    alert("unsuccessful");
     console.log(e);
   }
 }
@@ -75,7 +80,6 @@ export async function addMessage(message) {
 // add file
 export async function addFile(formData) {
   try {
-
     const responce = await axios.post(
       "https://localhost:5000/fileUploadRoute/fileAdd",
       formData,
@@ -86,10 +90,10 @@ export async function addFile(formData) {
         },
       }
     );
-    alert("file sent successfully")
+    alert("file sent successfully");
     console.log(responce);
   } catch (e) {
-    alert("unsuccessful")
+    alert("unsuccessful");
     console.log(e);
   }
 }
